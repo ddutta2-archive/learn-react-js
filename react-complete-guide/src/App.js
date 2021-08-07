f936a209f445ecfd9c95ab6140ee78cc9be591ab
debo12 <debojyotiduttac12@gmail.com>
2021-08-07 21:29:54 +0530

    adding javascript code into components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,10 +4,10 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expense = [
-    {'expenseDate': new Date(2021, 8, 6), 'expenseTitle': 'React JS Course', 'expensePrice': 150.12},
-    {'expenseDate': new Date(2020, 9, 3), 'expenseTitle': 'C# Course', 'expensePrice': 50.50},
-    {'expenseDate': new Date(2021, 4, 5), 'expenseTitle': 'Azure Developer Course', 'expensePrice': 200.45},
-    {'expenseDate': new Date(2021, 4, 2), 'expenseTitle': 'Machine Learning Course', 'expensePrice': 500},
+    { 'expenseDate': new Date(2021, 8, 6), 'expenseTitle': 'React JS Course', 'expensePrice': 150.12 },
+    { 'expenseDate': new Date(2020, 9, 3), 'expenseTitle': 'C# Developer Course', 'expensePrice': 50.50 },
+    { 'expenseDate': new Date(2021, 4, 5), 'expenseTitle': 'Azure Developer Course', 'expensePrice': 200.45 },
+    { 'expenseDate': new Date(2021, 4, 2), 'expenseTitle': 'Machine Learning Course', 'expensePrice': 500 },
   ];
   return (
     <div className="App">
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
+    const month = props.expenseDate.toLocaleString('en-US', { month: 'long' });
+    const date = props.expenseDate.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.expenseDate.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>{props.expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{date}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.expenseTitle}</h2>
                 <div className='expense-item__price'>${props.expensePrice}</div>
