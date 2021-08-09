f53b44a4daf86c1e8d5f1cee1d94abde8dec1364
debo12 <debojyotiduttac12@gmail.com>
2021-08-09 23:49:29 +0530

    alternative function syntax

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpenseItem expenseDate={props.expense.expenseDate} expenseTitle={props.expense.expenseTitle} expensePrice={props.expense.expensePrice} />
